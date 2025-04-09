7e96e2f646b654ae86d2e22c68375875b71150a2
Sushant Kate <47480733+Sushant98K@users.noreply.github.com>
2025-04-10 01:38:33 +0530

    Bootstrap integration

---
+++ Style/bootstrap/README.md
@@ -0,0 +1,13 @@
+This is a bootstrap setup with vite
+
+installation 
+
+    npm i bootstrap
+
+setup 
+project > src > main.jsx
+
+    import 'bootstrap/dist/css/bootstrap.min.css'
+    import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+all done
---
+++ Style/bootstrap/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+      <button type="button" className="btn btn-success">
+        Success
+      </button>
+      <button type="button" className="btn btn-danger">
+        danger
+      </button>
+      <button type="button" className="btn btn-primary">
+        primary
+      </button>
+    </>
+  );
+}
+
+export default App
